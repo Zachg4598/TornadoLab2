053086494d43f41c5577fce052f6034ee7e0b59c
Zach <thebatzach8@gmail.com>
2024-01-24 16:10:21 -0500

    final commit

--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
 import asyncio
 import tornado.web
-import Index
-import Quote
 from userProfiles import UserProfileHandler
+
 def makeApp():
-    endpoints=[
-        ("/",Index.Handler),
-        ("/quote",Quote.Handler)
-        (r"/profile/(\w+)",UserProfileHandler),
+    endpoints = [
+        (r"/profile/(\w+)", UserProfileHandler),  
     ]
     app = tornado.web.Application(endpoints)
     app.listen(8000)
@@ -16,3 +13,6 @@ def makeApp():
 if __name__ == "__main__":
     app = makeApp()
     asyncio.get_event_loop().run_forever()
+
+
+#final version
