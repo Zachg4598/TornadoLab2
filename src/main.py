8f2398b06021d532055dcfa3472bdb70695e3cc9
Zach <thebatzach8@gmail.com>
2024-01-24 15:42:24 -0500

    update user userProfiles

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import asyncio
+import tornado.web
+import Index
+import Quote
+from userProfiles import UserProfileHandler
+def makeApp():
+    endpoints=[
+        ("/",Index.Handler),
+        ("/quote",Quote.Handler)
+        (r"/profile/(\w+)",UserProfileHandler),
+    ]
+    app = tornado.web.Application(endpoints)
+    app.listen(8000)
+    return app
+
+if __name__ == "__main__":
+    app = makeApp()
+    asyncio.get_event_loop().run_forever()
